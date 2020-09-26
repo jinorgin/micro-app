65e99830eadad156fb8cd35a1809879fe8e653a2
Jin Kwan Kim <31860776+jinorgin@users.noreply.github.com>
2020-09-26 13:57:30 -0400

    Initialize 1-add-title

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  console.log("Rendering Component", new Date().getTime());
+
+  return (
+    <div className="App">
+      <h3>Quote Generator - React</h3>
+
+    </div>
+  );
+}
+
+export default App;
