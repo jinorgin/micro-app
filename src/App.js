7ed7fd1712d3ebf7f7717e2118b3dffbc06abeda
Jin Kwan Kim <31860776+jinorgin@users.noreply.github.com>
2020-09-26 13:57:34 -0400

    Initialize 13-use-axios

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  console.log("Rendering Component", new Date().getTime());
+
+  const initialData = [
+    "I change my locks every 16 days.",
+    "Creativity is for people with glasses who like to lie.",
+    "Cultivating a manly musk puts opponent on notice.",
+  ];
+
+  const [data, setData] = useState(initialData);
+
+  const listArray = data.map(item => {
+    return (<li>{item}</li>);
+  });
+
+  const addItem = function (event) {
+    const url = "http://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+    axios(url).then(result => {
+      setData(prev=>[...prev, ...data]);
+    });
+    
+  };
+
+  const clear = function () {
+    setData([]);
+  };
+
+  return (
+    <div className="App">
+      <h3>Quote Generator - React</h3>
+
+      <button type="button" onClick={addItem}>Add Item</button>
+      <span>&nbsp;</span>
+      <button id="clear" type="button" onClick={clear}>Clear List</button>
+
+      <ul>
+        {listArray}
+      </ul>
+
+    </div>
+  );
+}
+
+export default App;
